01bdb19f5f76e8d4e97745e949fce1906e11e81a
happytech <231250176@smail.nju.edu.cn>
2024-11-09 00:49:22 +0800

    add popweb support for latex preview and translation pop window

--- init.el
+++ init.el
@@ -915,6 +915,8 @@
                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
 
+(use-package ox-hugo
+  :ensure t)
 (use-package ox-gfm
   :ensure t
   :after org)
@@ -1395,3 +1397,29 @@
   (require 'eaf-evil)
   (require 'eaf-all-the-icons)
   ) ;; unbind, see more in the Wiki
+
+(defvar path-to-popweb "~/.emacs.d/pluginTools/popweb")
+(use-package org-transclusion
+  :ensure t)
+(use-package popweb
+  :load-path path-to-popweb
+  :config
+  (setq popweb-url-web-window-width-scale 1.5)
+  (setq popweb-url-web-window-height-scale 1.5)
+  ;; Org-Roam ID link and footnote link previewer
+  (add-to-list 'load-path (concat path-to-popweb "/extension/org-roam"))
+  (require 'popweb-org-roam-link)
+
+  ;; LaTeX preview functionality
+  (add-to-list 'load-path (concat path-to-popweb "/extension/latex"))
+  (require 'popweb-latex)
+  (add-hook 'latex-mode-hook #'popweb-latex-mode)
+
+  ;; Chinese-English translation popup
+  (add-to-list 'load-path (concat path-to-popweb "/extension/dict")) ;
+  (require 'popweb-dict)
+
+  ;; Anki note review popup
+  (add-to-list 'load-path (concat path-to-popweb "/extension/anki-review"))
+  (require 'popweb-anki-review)
+  )
