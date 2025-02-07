2cbd442a399bdbe7f4e895aff7bf9c1f57ec23c5
happytech <happytech.web@gmail.com>
2025-02-07 18:39:30 +0800

    adapt nix os, add .gitignore

--- elpa/uniline-20250204.820/uniline-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "uniline" "20250204.820"
-  "Add UNICODE based diagrams to text files."
-  '((emacs "29.1")
-    (hydra "0.15.0"))
-  :url "https://github.com/tbanel/uniline"
-  :commit "98ff6c4586fbe908c81ef8910b11b111c8cc31af"
-  :revdesc "98ff6c4586fb"
-  :keywords '("convenience" "text"))
