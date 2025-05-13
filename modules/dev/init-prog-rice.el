47b31f5fde70c014c51aee488da46adf49de8857
happytech <happytech.web@gmail.com>
2025-05-13 11:51:46 +0800

    deprecated init-prog-rice.el

--- modules/dev/init-prog-rice.el
+++
@@ -1,8 +0,0 @@
-;; rainbow bracket
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-(setq-default bidi-paragraph-direction 'left-to-right)
-
-(provide 'init-prog-rice)
-;;; init-prog-rice.el ends here
