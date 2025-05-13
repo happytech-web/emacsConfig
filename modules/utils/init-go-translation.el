081464c55a0a5aba5ae1f20a33a288927f73d07c
happytech <happytech.web@gmail.com>
2025-05-13 11:56:11 +0800

    add fanyi support

--- modules/utils/init-go-translation.el
+++ modules/utils/init-go-translation.el
@@ -1,11 +1,19 @@
 (use-package go-translate
   :ensure t
+  :general
+  (rune/leader-keys
+    "l" '(:ignore t :which-key "language")
+    "ll" '(gt-do-translate :which-key "go translation"))
   :config
   (setq gt-langs '(en zh))
   (setq gt-default-translator (gt-translator :engines (gt-youdao-dict-engine))))
 
-(rune/leader-keys
-  "l" '(gt-do-translate :which-key "translation"))
+(use-package fanyi
+  :straight t
+  :general
+  (rune/leader-keys
+    "la" '(fanyi-dwim2 :which-key "fanyi-dwim2")))
+
 
 (provide 'init-go-translation)
 ;;; init-go-translation.el ends here
